1cc565358e93944a00d42422c24c1b8da8995329
Maple8192 <57043077+Maple8192@users.noreply.github.com>
2021-07-21 00:10:46 +0900

    命令をenum化

--- scripts/script.js
+++ scripts/script.js
@@ -10,6 +10,16 @@
 . -> 18
 */
 
+const Commands = {
+    add: 0,
+    sub: 1,
+    sft: 2,
+    usf: 3,
+    sta: 4,
+    end: 5,
+    out: 6
+}
+
 function run() {
     const code = load()
 
@@ -33,31 +43,31 @@ function interpret(code) {
     for(let i = 1; i <= code.length; i++) {
         switch(code.substring(p, i)) {
             case "Tani":
-                commands.push("Tani")
+                commands.push(Commands.add)
                 p = i
                 break
             case "Tako":
-                commands.push("Tako")
+                commands.push(Commands.sub)
                 p = i
                 break
             case "yan":
-                commands.push("yan")
+                commands.push(Commands.sft)
                 p = i
                 break
             case "たに":
-                commands.push("たに")
+                commands.push(Commands.usf)
                 p = i
                 break
             case "やん":
-                commands.push("やん")
+                commands.push(Commands.sta)
                 p = i
                 break
             case "椛谷":
-                commands.push("椛谷")
+                commands.push(Commands.end)
                 p = i
                 break
             case "18":
-                commands.push("18")
+                commands.push(Commands.out)
                 p = i
                 break
         }
